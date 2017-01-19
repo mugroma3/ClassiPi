7d1d5a1284992f397cfd41e6867312905b92647e
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2017-01-19 23:04:37 +0100

    added some controls and test stuff

--- Classifier/ClassiPi.py
+++ Classifier/ClassiPi.py
@@ -1,17 +1,12 @@
-#%matplotlib inline
-#import matplotlib.pyplot as plt
 import sys
 import tensorflow as tf
 import numpy as np
 import os
 
-#from util import Util
-#u = Util()
-
-# Functions and classes for loading and using the Inception model.
+# Functions and classes for loading and using the Inception model v3.
 import inception
 
-tf.__version__
+print ('Tensorflow version ', tf.__version__)
 
 inception.maybe_download()
 
@@ -24,19 +19,23 @@ def classify(image_path):
     pred = model.classify(image_path=image_path)
     model.print_scores(pred=pred, k=10, only_first_name=True)
 
-
 def main():
   if len(sys.argv) >= 2:
     image_path = sys.argv[1]
   else:
-    image_path = ''
-  
-  print ("Classifying image", image_path)
+    print ('###### error ###### you need to specify a valid image')
+    sys.exit(1)
+  if not os.path.exists(file_path):
+    print ('###### error ###### not a valid file path')
+    sys.exit(1)
+
+  # We use the test image that comes with inception model v3 if arg 1 is 'test'
+  if sys.argv[1] == 'test':
+    image_path = os.path.join(inception.data_dir, 'cropped_panda.jpg')
+
+  print ("Classifying image ", image_path)
   print ('###### results ######')
   classify(image_path)
 
-
 if __name__ == '__main__':
   main()
-
-
