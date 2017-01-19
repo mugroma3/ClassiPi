0f2d16f2807009eb239c87dddcd19573b396dffd
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2017-01-20 00:06:12 +0100

    showing errors now

--- Classifier/ClassiPi.py
+++ Classifier/ClassiPi.py
@@ -1,24 +1,9 @@
 import sys
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 # Functions and classes for loading and using the Inception model v3.
 import inception
-
-def main():
-  if len(sys.argv) >= 2:
-    image_path = sys.argv[1]
-  else:
-    print ('###### error ######')
-    sys.exit('you need to specify a valid image')
-
-  # We use the test image that comes with inception model v3 if arg 1 is 'test'
-  if sys.argv[1] == 'test':
-    image_path = os.path.join(inception.data_dir, 'cropped_panda.jpg')
-
-  if not os.path.exists(image_path):
-    print ('###### error ######')
-    sys.exit('not a valid file path')
 
 # Load the Inception model so it is ready for classifying images.
 try:
@@ -30,9 +15,26 @@ except FileNotFoundError:
 
 # Helper-function for classifying and plotting images
 def classify(image_path):
-  #display(Image(image_path))
-  pred = model.classify(image_path=image_path)
-  model.print_scores(pred=pred, k=10, only_first_name=True)
+    #display(Image(image_path))
+    pred = model.classify(image_path=image_path)
+    model.print_scores(pred=pred, k=10, only_first_name=True)
+
+def main():
+  if len(sys.argv) >= 2:
+    image_path = sys.argv[1]
+  else:
+    model.close()
+    print ('###### error ######')
+    sys.exit('you need to specify a valid image')
+
+  # We use the test image that comes with inception model v3 if arg 1 is 'test'
+  if sys.argv[1] == 'test':
+    image_path = os.path.join(inception.data_dir, 'cropped_panda.jpg')
+
+  if not os.path.exists(image_path):
+    model.close()
+    print ('###### error ######')
+    sys.exit('not a valid file path')
 
   print ("Classifying image ", image_path)
   print ('###### results ######')
@@ -40,7 +42,6 @@ def classify(image_path):
   with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     classify(image_path)
-  model.close()
 
 if __name__ == '__main__':
   main()
