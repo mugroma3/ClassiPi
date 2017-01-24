d00ec6bb88ebd65e80bbec3f7640a21a002f99a4
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2017-01-24 19:49:18 +0100

    attempt to delay camera shoots and use image from ram

--- Classifier/ClassiPiLoopCamera.py
+++ Classifier/ClassiPiLoopCamera.py
@@ -13,9 +13,12 @@ import pygame.camera
 image_path = 'image.jpg'
 
 # Helper-function for classifying and plotting images
-def classify(model, image_path):
+def classify(model, image_path, image):
     #display(Image(image_path))
-    pred = model.classify(image_path=image_path)
+    if image_path:
+        pred = model.classify(image_path=image_path)
+    else:
+        pred = model.classify(image=image)
     # clear commands
     os.system('cls' if os.name == 'nt' else 'clear')
     print ('###### results ######')
@@ -24,10 +27,12 @@ def classify(model, image_path):
 def main():
   # getting image from camera
   pygame.camera.init()
-  img = 0
   #pygame.camera.list_camera() #Camera detected or not
   cam = pygame.camera.Camera("/dev/video0",(640,480))
-  
+  cam.start()
+  fps = 30.0
+  pygame.time.delay(int(1000 * 1.0/fps))
+
   # Load the Inception model so it is ready for classifying images.
   try:
     model = inception.Inception()
@@ -41,31 +46,28 @@ def main():
     start_time = time.time()
     
     #os.remove(image_path)
-    cam.start()
+
     if img == 0:
       img = cam.get_image()
     else:
       img = cam.get_image(img)
-    pygame.image.save(img,image_path)
-    cam.stop()
 
-    if not os.path.exists(image_path):
-      print ('###### error ######')
-      print('not a valid file path')
-      continue
-
-    print ("Classifying image ", image_path)
+    print ("Classifying image...")
     
     import warnings
     with warnings.catch_warnings():
       warnings.simplefilter("ignore")
-      classify(model, image_path)
+      classify(model, image=img)
     end_time = time.time()
     time_dif = end_time - start_time
     # Print the time-usage.
     print ('###### time usage ######')
     print(str(timedelta(seconds=int(round(time_dif)))))
 
+    # Save the image that was just classified (for debug)
+    pygame.image.save(img, image_path)
+
+  cam.stop()
   model.close()
 
 if __name__ == '__main__':
