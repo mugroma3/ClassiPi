f29925d43c6ced55f71ba7480ca1d6676a61b523
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2017-01-24 16:19:34 +0000

    fixed some errors, everything working fine

--- Classifier/ClassiPiLoopCamera.py
+++ Classifier/ClassiPiLoopCamera.py
@@ -7,7 +7,8 @@ from datetime import timedelta
 # Functions and classes for loading and using the Inception model v3.
 import inception
 
-from SimpleCV import Image, Camera
+import pygame
+import pygame.camera
 
 image_path = 'image.jpg'
 
@@ -15,9 +16,18 @@ image_path = 'image.jpg'
 def classify(model, image_path):
     #display(Image(image_path))
     pred = model.classify(image_path=image_path)
+    # clear commands
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print ('###### results ######')
     model.print_scores(pred=pred, k=10, only_first_name=True)
 
 def main():
+  # getting image from camera
+  pygame.camera.init()
+  img = 0
+  #pygame.camera.list_camera() #Camera detected or not
+  cam = pygame.camera.Camera("/dev/video0",(640,480))
+  
   # Load the Inception model so it is ready for classifying images.
   try:
     model = inception.Inception()
@@ -29,11 +39,15 @@ def main():
   while True:
     print ('###### input ######')
     start_time = time.time()
-
-    # getting image from camera
-    cam = Camera()
-    img = cam.getImage()
-    img.save(image_path)
+    
+    #os.remove(image_path)
+    cam.start()
+    if img == 0:
+      img = cam.get_image()
+    else:
+      img = cam.get_image(img)
+    pygame.image.save(img,image_path)
+    cam.stop()
 
     if not os.path.exists(image_path):
       print ('###### error ######')
@@ -41,9 +55,7 @@ def main():
       continue
 
     print ("Classifying image ", image_path)
-    # clear commands
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print ('###### results ######')
+    
     import warnings
     with warnings.catch_warnings():
       warnings.simplefilter("ignore")
