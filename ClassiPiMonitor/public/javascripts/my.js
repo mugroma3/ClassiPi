811c9197525170da1b4b7562c21389f3218559e1
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2017-02-05 17:11:21 +0100

    changed date format

--- ClassiPiMonitor/public/javascripts/my.js
+++ ClassiPiMonitor/public/javascripts/my.js
@@ -1,6 +1,6 @@
 var socket = io();
 socket.on('newImage', function (msg) {
-	document.getElementById("image").src = "watching/image.jpg?" + new Date();
+	document.getElementById("image").src = "watching/image.jpg?" + new Date().getTime();
 });
 socket.on('newClass', function (msg) {
 	document.getElementById("classification").innerHTML = msg;
