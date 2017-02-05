006616d9d96999e7501cf096a642545aa2462db1
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2017-02-05 19:28:41 +0100

    better looking output

--- ClassiPiMonitor/public/javascripts/my.js
+++ ClassiPiMonitor/public/javascripts/my.js
@@ -1,7 +1,16 @@
+function addBold(text) {
+	return text.replace(/^###### /g, "<b>")
+			   .replace(/\n###### /g, "\n\n<b>")
+			   .replace(/######/g, "</b>");
+}
+
+document.getElementById("classification").innerHTML = addBold(document.getElementById("classification").innerHTML);
+
 var socket = io();
 socket.on('newImage', function (msg) {
 	document.getElementById("image").src = "watching/image.jpg?" + new Date().getTime();
 });
 socket.on('newClass', function (msg) {
-	document.getElementById("classification").innerHTML = msg;
+	document.getElementById("classification").innerHTML = addBold(msg);
 });
+
