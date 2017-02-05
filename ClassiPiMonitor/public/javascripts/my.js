44c1a84f8d3a7154f143a1748171b9a47a547251
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2017-02-05 19:38:10 +0100

    relative paths

--- ClassiPiMonitor/public/javascripts/my.js
+++ ClassiPiMonitor/public/javascripts/my.js
@@ -8,7 +8,7 @@ document.getElementById("classification").innerHTML = addBold(document.getElemen
 
 var socket = io();
 socket.on('newImage', function (msg) {
-	document.getElementById("image").src = "watching/image.jpg?" + new Date().getTime();
+	document.getElementById("image").src = "/watching/image.jpg?" + new Date().getTime();
 });
 socket.on('newClass', function (msg) {
 	document.getElementById("classification").innerHTML = addBold(msg);
